bf0b3e42c4894d8b4526e898bc4b9c370f66bd60
Andrew T <andrew.t@adaas.org>
2024-06-07 14:27:43 +0300

    new version created ::  0.0.3

--- dist/src/types/ADAAS_A_AUTH_APIProvider.types.d.ts
+++
@@ -1,6 +0,0 @@
-export type ADAAS_A_AUTH_TYPES__APIProviderError = {
-    name: string;
-    code: string;
-    description?: string;
-    message: string;
-};
--- dist/src/types/ADAAS_A_AUTH_APIProvider.types.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=ADAAS_A_AUTH_APIProvider.types.js.map
